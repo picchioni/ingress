ee2b73d5750551049c160e95f4058e0fa9d8d973
Garret Picchioni <gpicchioni@trustwave.com>
2013-10-06 13:42:01 -0700

    Initial commit

---
+++ ingressDownload.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Install IOS-Ingress direct download on your device
+#Hacked together by Garret Picchioni
+#Requires jailbroken device with wget, AppCake, and a way to execute this script (via terminal or SSH)
+#Should be executed as mobile user, but will compensate if ran by root
+#Script can be placed anywhere but /var/mobile is suggested
+
+downloadsFolder="/var/mobile/Documents/Downloads/"
+ingressIPA="${downloadsFolder}Ingress.ipa"
+remoteLocation="http://www.ios-ingress.com/downloads/Ingress.ipa"
+
+echo "Checking if wget is installed"
+wgetCheck=`type -P wget`
+if [[ -z $wgetCheck ]]; then
+	echo "wget is not installed, please install via Cydia"
+	exit 1
+fi
+
+if [[ -f $ingressIPA ]]; then
+	echo "Removing old Ingress IPA file"
+	rm $ingressIPA
+fi
+
+echo "Downloading Ingress"
+wget -P $downloadsFolder $remoteLocation
+
+user=`whoami`
+if [[ $user != "mobile" ]]; then
+	echo "Setting proper permissions for the IPA"
+	chown mobile $ingressIPA
+fi
+
+echo "Done! Install IPA from AppCake"
